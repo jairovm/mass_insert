d959669bda4a6f494a7ed5f506ec7b4480ea839c
Jairo Vazquez <jairovm20@gmail.com>
2017-05-10 09:58:03 -0700

    Adds support to assign primary key manually

--- lib/mass_insert/adapters/mysql2_adapter.rb
+++ lib/mass_insert/adapters/mysql2_adapter.rb
@@ -6,7 +6,7 @@ module MassInsert
       end
 
       def on_duplicate_key_update
-        if @options[:handle_duplication]
+        if options[:handle_duplication]
           "ON DUPLICATE KEY UPDATE #{on_duplicate_key_update_values}"
         end
       end
